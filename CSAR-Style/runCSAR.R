45f216f5488ed5539748ac8dfaa30a357e8f315a
Dermot Harnett <zslastman@gmail.com>
2020-07-02 14:26:23 +0200

    updated CSAR-Style to filter unmapped reads, do the bedtools stuff itself

--- CSAR-Style/runCSAR.R
+++ CSAR-Style/runCSAR.R
@@ -5,8 +5,13 @@ library(foreach)
 library(doParallel)
  "rpotmp_early_sample2"
  setwd(here("CSAR-Style"))
+
+ bamfolder = "/Users/dharnet/Dropbox/peak_data/"
+ outfolder='bed-Step1/'
+ stopifnot(file.exists(bamfolder))
+ dir.create(outfolder,showWarn=FALSE)
  
- 
+
  exportenv <- function(){
        fenv <- parent.frame()
        for(i in ls(envir=fenv)){
@@ -17,21 +22,28 @@ library(doParallel)
 #BiocManager::install(c('doParallel','CSAR','GenomeInfoDb','foreach'))
 #This is jose's function that runs CSAR on a single bam file 
 run<-function(name,rep,n=-1,b=2,w=1L,nper=100){
-  bam<-paste("../bamFiles/",name,".bam",sep="")
+  bam<-paste(bamfolder,name,".bam",sep="")
   stopifnot(file.exists(bam))
   #read in the bam data as a list
   bam <- scanBam(bam,param=param)[[1]];names(bam)<-c("chr","strand","pos","lengthRead","mq");
+  tail(as.data.frame(bam))
   #copy this data for some reason
+
   bam2<-as.data.frame(bam);bam2$Nhits=1;
+  #! filter out the bam lines that have no value for 'chr' (unmapped reads)
+  bam2 <- bam2[!is.na(bam2[['chr']]),]
   bam2$pos<-bam2$pos-1##to put it in 0-based coordinates (as genome browser)
   bam2$pos[bam2$strand=="-"]<-bam2$pos[bam2$strand=="-"]+bam2$lengthRead[bam2$strand=="-"]-1
+
   if(is.null(bam))return(NA)
   ip=bam2
   #Now get the control bam for this sample
   name<-sub("sample","control",name)
-  controlbam <- paste("../bamFiles/",name,".bam",sep="")
+  controlbam <- paste(bamfolder,name,".bam",sep="")
   bam <- scanBam(controlbam,param=param)[[1]];names(bam)<-c("chr","strand","pos","lengthRead","mq");
   bam1<-as.data.frame(bam);bam1$Nhits=1;
+  #! filter out the bam lines that have no value for 'chr' (unmapped reads)
+  bam1 <- bam1[!is.na(bam1[['chr']]),]
   bam1$pos<-bam1$pos-1##to put it in 0-based coordinates (as genome browser)
   bam1$pos[bam1$strand=="-"]<-bam1$pos[bam1$strand=="-"]+bam1$lengthRead[bam1$strand=="-"]-1
   control<-bam1
@@ -74,7 +86,7 @@ run<-function(name,rep,n=-1,b=2,w=1L,nper=100){
       win1$name<-".";win1$score<-"."
       if(strand=="Foward"){win1$strand<-"+"}
       if(strand=="Reverse"){win1$strand<-"-"}
-      write.table(win1,file=paste("bed-Step1/TestRatio-",name,"Strand",strand,n,"b",b,"w",w,"minl",minlength,".bed",sep=""),quote=F,sep="\t",col.names=F,row.names=F)
+      write.table(win1,file=paste(outfolder,"TestRatio-",name,"Strand",strand,n,"b",b,"w",w,"minl",minlength,".bed",sep=""),quote=F,sep="\t",col.names=F,row.names=F)
       }
       
     }
@@ -85,6 +97,7 @@ run<-function(name,rep,n=-1,b=2,w=1L,nper=100){
 }
 
 ids<-list.files(pattern=".bam$","../bamFiles")
+ids<-list.files(pattern=".bam$",bamfolder)
 ids<-unique(sub(".bam","",ids))
 ids<-ids[grep("sample",ids)]
 #run(name="wt_late_sample1",rep=1,nper=2)
@@ -97,16 +110,31 @@ param <- ScanBamParam( what = what)
 for(nam in ids){print(nam);run(name=nam,rep=1)}
 stopCluster(cl)
  
-##merge with script at run,sh
-q1<-read.table("bed-Step1/merged-1b2w1minl0.bed");q1$V1<-as.character(q1$V1)
+# ##merge with script at run,sh
+#   #run in shell
+# cd /scratch/AG_Ohler/jmuino/Julia/Tex-experiment/CSAR-Style/bed-Step1
+# rm merged*.bed
+# cat *.bed >> merged.bed
+# sort  -k1,1 -k2,2n -i merged.bed > mergeds.bed
+#  mergeBed -i mergeds.bed -iobuf 5G -s  >merged-1b2w1minl0.bed
+
+#  ##Look in DESEq2-analysis.R how to get fasta
+# bedtools getfasta -s -fi /scratch/AG_Ohler/jmuino/genomes/TAIR9/chr-TAIR9.fas -bed merged-1b2w1minl0.bed > merged-1b2w1minl0.bed
+
+system(paste0('cat ',paste(list.files(outfolder,full=T),collapse=' '),'>> merged.bed'))
+system('sort  -k1,1 -k2,2n -i merged.bed > mergeds.bed')
+if(system('mergeBed')==0)stop('need to install mergeBed with e.g. conda install bedtools ')
+system('mergeBed -i mergeds.bed -iobuf 5G -s  >merged-1b2w1minl0.bed')
+
+q1<-read.table(file.path(outfolder,"merged-1b2w1minl0.bed"));q1$V1<-as.character(q1$V1)
 #q1<-rbind(q1,c("spike-in",1,485,"+"))
 gtf<-data.frame(q1$V1,"CSAR","gene",q1$V2,q1$V3,"1000",q1$V4,".",paste("gene_id \"",q1$V2,"\"; transcript_id \"",q1$V2,"\";",sep=""))
 gtf[,9]<-as.character(gtf[,9]);gtf[,7]<-as.character(gtf[,7])
 #gtf[length(gtf[,1]),9]<-paste("gene_id \"spike-inJose\"; transcript_id \"spike-inJose\";",sep="")
-write.table(gtf,file="bed-Step1/merged-1b2w1minl0.gtf",quote=F,row.names=F,col.names=F,sep="\t")
+write.table(gtf,file=file.path(outfolder,"merged-1b2w1minl0.gtf"),quote=F,row.names=F,col.names=F,sep="\t")
 
 q1<-read.table("merged-1b2w1minl0.bed");q1<-q1[q1$V1=="ChrC",]
-gff<-read.table("/scratch/AG_Ohler/jmuino/genomes/TAIR9/Araport11_GFF3_genes_transposons.201606.gtf",sep="\t")
+gff<-read.table("ext_data/Araport11_GFF3_genes_transposons.201606.gtf",sep="\t")
 gff1<-gff[gff$V1=="ChrC" & gff$V3=="gene",]
 disF<-sapply(q1$V3[q1$V4=="+"],function(x){temp<-gff1$V4[gff1$V7=="+"]-x;c(as.character(gff1$V9[gff1$V7=="+"])[temp==min(temp[temp>0 & temp <1000])],NA)[1]})
 disR<-sapply(q1$V2[q1$V4=="-"],function(x){temp<-gff1$V5[gff1$V7=="-"]-x;c(as.character(gff1$V9[gff1$V7=="-"])[temp==max(temp[temp<0 & temp > -1000])],NA)[1]})
